3be6a4fb4de048df88558a4e2fde2fda32596a32
majid <majidvayeghani@gmail.com>
2022-07-25 17:39:41 +0430

    fix a mistake

--- kosar/kosar/settings.py
+++ kosar/kosar/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UserConfig',
-    'users',
+    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
