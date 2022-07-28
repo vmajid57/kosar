b2abf02a122b6e7ea172ff656a6dc0449e6cab0e
majid <majidvayeghani@gmail.com>
2022-07-28 16:10:51 +0430

    fix some bugs and register LoanAdmin

--- kosar/account/admin.py
+++ kosar/account/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Loan
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+    list_display = ('username', 'age', 'phone_number')
+
+
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('customUser', 'amount',)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Loan, LoanAdmin)
--- kosar/account/models.py
+++ kosar/account/models.py
@@ -9,12 +9,12 @@ class CustomUser(AbstractUser):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits"
                                          " allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True, default=None, null=False)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, unique=True, null=True)
 
 
 class Loan(models.Model):
-    # customUser = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    customUser = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     amount = models.IntegerField()
     created_at = models.DateTimeField(default=None)
     end_at = models.DateTimeField(default=None)
-    numberOf = models.DateTimeField(default=30)
+    numberOf = models.IntegerField(default=30)
