5b40448dc55f501e540930f955d38bc9833b8310
majid <majidvayeghani@gmail.com>
2022-07-31 18:01:47 +0430

    register ProfileAdmin

--- kosar/account/admin.py
+++ kosar/account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Loan
+from .models import CustomUser, Loan, Profile
 
 
 class CustomUserAdmin(admin.ModelAdmin):
@@ -10,5 +10,10 @@ class LoanAdmin(admin.ModelAdmin):
     list_display = ('customUser', 'amount',)
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Loan, LoanAdmin)
+admin.site.register(Profile, ProfileAdmin)
