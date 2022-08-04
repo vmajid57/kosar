eaf9be8087aeb099365d3ae8cf53f89cd45a71a5
majid <majidvayeghani@gmail.com>
2022-08-04 13:31:57 +0430

    set MEDIA_URL and MEDIA_ROOT for User upload files/working with images

--- kosar/kosar/settings.py
+++ kosar/kosar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # redirect to the home page after a successful login
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
